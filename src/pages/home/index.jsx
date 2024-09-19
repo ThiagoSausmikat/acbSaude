a00054625dbdebf5a1647c84285c78529317722b
ThiagoSausmikat <tsmoura.93@gmail.com>
2024-09-18 22:50:55 -0300

    voltu para manutenção

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -9,6 +9,9 @@ import Sessao1 from '../../componentes/Home/Sessao1';
 import Sessao2 from '../../componentes/Home/Sessao2';
 import Footer from '../../componentes/Footer';
 
+import desktop from '../../assets/manutencao.jpg'
+import mobile from '../../assets/manutencaoMobile.jpg'
+
 const Home = () => {
   const location = useLocation();
 
@@ -18,12 +21,13 @@ const Home = () => {
 
   return (
     <div className={styles.body} id='inicio'>
-      <Navbar />
-      <Carousel1 />
-      <Sessao1 />
-      <ParallaxSection />
-      <Sessao2 />
-      <Footer />
+ 
+      <div className={styles.desktopOnly}>
+      <img className={styles.foto} src={desktop} alt="Logo" />
+      </div>
+      <div className={styles.mobileOnly}>
+      <img className={styles.foto} src={mobile} alt="Logo" />
+      </div>
     </div>
   );
 };
