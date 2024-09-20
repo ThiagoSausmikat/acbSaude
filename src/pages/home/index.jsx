3b17607e7d0ccb732b334aa57cf9ef5f30b7dc39
ThiagoSausmikat <tsmoura.93@gmail.com>
2024-09-19 23:20:22 -0300

    em manutenção de novo

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,31 +1,24 @@
-import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+
+import React from 'react';
+
 import styles from "./styles.module.css";
 
-import Carousel1 from '../../componentes/NovoCarrossel';
-import Navbar from '../../componentes/NavBar';
-import ParallaxSection from '../../componentes/Paralax';
-import Sessao1 from '../../componentes/Home/Sessao1';
-import Sessao2 from '../../componentes/Home/Sessao2';
-import Footer from '../../componentes/Footer';
+
+import desktop from '../../assets/manutencao.jpg'
+import mobile from '../../assets/manutencaoMobile.jpg'
 
 const Home = () => {
-  
- 
-  const location = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location]);
 
   return (
     <div className={styles.body} id='inicio'>
-      <Navbar />
-      <Carousel1 />
-      <Sessao1 />
-      <ParallaxSection />
-      <Sessao2 />
-      <Footer />
+    
+ 
+      <div className={styles.desktopOnly}>
+      <img className={styles.foto} src={desktop} alt="Logo" />
+      </div>
+      <div className={styles.mobileOnly}>
+      <img className={styles.foto} src={mobile} alt="Logo" />
+      </div>
     </div>
   );
 };
